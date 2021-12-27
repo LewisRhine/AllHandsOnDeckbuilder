ae9bca18697d0b3408f81115d67a173b28e9782f
JonR205 <57925619+JonR205@users.noreply.github.com>
2021-12-27 16:29:55 -0600

    Added cards (#2)
    * Initital push
    
    * Original cards classes added
    
    * Added base card classes and started cards
    
    * Removing venv
    
    * Cleaned up instance calls
    
    * Delete ahodb directory

---
+++ classes/Cards/base_card.py
@@ -0,0 +1,59 @@
+from enum import Enum
+
+
+class CardType(Enum):
+    attack = 1
+    move = 2
+    defense = 3
+
+
+class BaseCard:
+    def __init__(self, type: CardType, cost_to_play: int, name: str, effect_text: str):
+        self.type = type
+        self.cost_to_play = cost_to_play
+        self.name = name
+        self.effect_text = effect_text
+
+
+class FireTorpedoes(BaseCard):
+    def __init__(
+        self,
+        type=CardType.attack,
+        cost_to_play=1,
+        name="fire torpedoes!",
+        effect_text="Target that explosion and fire!",
+    ):
+        super().__init__(type, cost_to_play, name, effect_text)
+
+
+class FireLasers(BaseCard):
+    def __init__(
+        self,
+        type=CardType.attack,
+        cost_to_play=1,
+        name="Fire Lasers!",
+        effect_text="Activate Laser attack for ONE round",
+    ):
+        super().__init__(type, cost_to_play, name, effect_text)
+
+
+class PowerToTheEngines(BaseCard):
+    def __init__(
+        self,
+        type=CardType.move,
+        cost_to_play=1,
+        name="Power to the engines!",
+        effect_text="I am a leaf on the wind... Watch how I soar…",
+    ):
+        super().__init__(type, cost_to_play, name, effect_text)
+
+
+class ShieldsUp(BaseCard):
+    def __init__(
+        self,
+        type=CardType.defense,
+        cost_to_play=1,
+        name="Shields Up!",
+        effect_text="Plus 5 to shields",
+    ):
+        super().__init__(type, cost_to_play, name, effect_text)
