7988f4d0d1ba852d54d1410c5431048f13fa7864
LewisRhine <lewisrhine@gmail.com>
2021-12-28 09:51:57 -0600

    Change how the ships power systems work and make thing more private in the class

--- classes/ship.py
+++ classes/ship.py
@@ -4,62 +4,99 @@ import math, pygame
 
 
 class Ship(pygame.sprite.Sprite):
-    def __init__(self, x: int, y: int, max_engine_power: int, max_shielding_power: int, rotations: int = 360):
+    def __init__(self, x: int, y: int, power_allotment: int, rotations: int = 360):
         super().__init__()
-        self.max_engine_power = max_engine_power
-        self.current_engine_power = self.max_engine_power
-        self.max_shielding_power = 0
-        self.current_shielding_power = max_shielding_power
-        self.accelerating: bool = False
-        self.reversing: bool = False
-        self.rot_img = []
-        self.min_angle = (360 / rotations)
+        self._power_allotment: int = power_allotment
+        self._engine_power: int = 0
+        self._shielding_power: int = 0
+        self._weapon_power: int = 0
+        self._accelerating: bool = False
+        self._reversing: bool = False
+        self._rot_img = []
+        self._min_angle = (360 / rotations)
         for i in range(rotations):
-            rotated_image = pygame.transform.rotozoom(self.image, 360 - 90 - (i * self.min_angle), 1)
-            self.rot_img.append(rotated_image)
-        self.min_angle = math.radians(self.min_angle)
-        self.image = self.rot_img[0]
+            rotated_image = pygame.transform.rotozoom(self.image, 360 - 90 - (i * self._min_angle), 1)
+            self._rot_img.append(rotated_image)
+        self._min_angle = math.radians(self._min_angle)
+        self.image = self._rot_img[0]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
-        self.heading = 0
-        self.speed = 0
-        self.velocity = pygame.math.Vector2(0, 0)
-        self.position = pygame.math.Vector2(x, y)
+        self._heading = 0
+        self._speed = 0
+        self._velocity = pygame.math.Vector2(0, 0)
+        self._position = pygame.math.Vector2(x, y)
+
+    # ----------------------------------------------------
+    # these are read only props because we want things outside this class to only be able to use the "power_to" method
+    # to set them. That way the class has control over checking that there is enough left in power_allotment
+    # and subtracting it when putting power into other systems
+    @property
+    def power_allotment(self):
+        return self._power_allotment
+
+    @property
+    def engine_power(self):
+        return self._engine_power
+
+    @property
+    def shielding_power(self):
+        return self._shielding_power
+
+    @property
+    def weapon_power(self):
+        return self._weapon_power
+
+    # ---------------------------------------------------
+
+    def power_to_shielding(self, amount: int):
+        if amount > self.power_allotment: return
+        self._shielding_power += amount
+        self._power_allotment -= amount
+
+    def power_to_engines(self, amount: int):
+        if amount > self.power_allotment: return
+        self._engine_power += amount
+        self._power_allotment -= amount
+
+    def power_to_weapons(self, amount: int):
+        if amount > self.power_allotment: return
+        self._weapon_power += amount
+        self._power_allotment -= amount
 
     def turn_left(self):
-        self.turn(-self.current_engine_power / 2)
+        self.__turn(-self._engine_power / 2)
 
     def turn_right(self):
-        self.turn(self.current_engine_power / 2)
+        self.__turn(self._engine_power / 2)
 
-    def turn(self, angle_degrees: float):
-        self.heading += math.radians(angle_degrees)
-        image_index = int(self.heading / self.min_angle) % len(self.rot_img)
-        if self.image != self.rot_img[image_index]:
+    def __turn(self, angle_degrees: float):
+        self._heading += math.radians(angle_degrees)
+        image_index = int(self._heading / self._min_angle) % len(self._rot_img)
+        if self.image != self._rot_img[image_index]:
             x, y = self.rect.center
-            self.image = self.rot_img[image_index]
+            self.image = self._rot_img[image_index]
             self.rect = self.image.get_rect()
             self.rect.center = (x, y)
 
     def accelerate(self):
-        self.accelerating = True
-        if not self.speed >= self.current_engine_power:
-            self.speed += 0.05
+        self._accelerating = True
+        if not self._speed >= self._engine_power:
+            self._speed += 0.05
 
     def reverse(self):
-        self.reversing = True
-        if not self.speed <= -self.current_engine_power:
-            self.speed -= 0.05
+        self._reversing = True
+        if not self._speed <= -self._engine_power:
+            self._speed -= 0.05
 
     def brake(self):
-        self.accelerating = False
-        self.reversing = False
+        self._accelerating = False
+        self._reversing = False
 
     def update(self):
-        self.velocity.from_polar((self.speed, math.degrees(self.heading)))
-        self.position += self.velocity
-        self.rect.center = (round(self.position[0]), round(self.position[1]))
-        if self.speed > 0 and not self.accelerating:
-            self.speed -= 0.05
-        if self.speed < 0 and not self.reversing:
-            self.speed += 0.05
+        self._velocity.from_polar((self._speed, math.degrees(self._heading)))
+        self._position += self._velocity
+        self.rect.center = (round(self._position[0]), round(self._position[1]))
+        if self._speed > 0 and not self._accelerating:
+            self._speed -= 0.05
+        if self._speed < 0 and not self._reversing:
+            self._speed += 0.05
